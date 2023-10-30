57eef2d4cae25adf5bd881d117ae0ea21750dd67
Zachary Lau <zlau2000@gmail.com>
2023-10-29 21:12:59 -0600

    Finalize input labels

--- importapp/main.js
+++ importapp/main.js
@@ -1,13 +1,16 @@
 const {app, BrowserWindow} = require('electron')
+const electronReload = require('electron-reload');
 const path = require('node:path')
+
+electronReload(__dirname);
+if (module.hot){
+    module.hot.accept();
+}
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
-        }
     });
     win.loadFile('index.html');
 }
