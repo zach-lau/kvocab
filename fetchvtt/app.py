5df17e959c98b77c444daa43bfe47d431265562b
Zachary Lau <zlau2000@gmail.com>
2023-11-01 20:15:02 -0600

    Update filename and language handling

--- fetchvtt/app.py
+++ fetchvtt/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
 import requests
+import os
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True, origins=["https://www.viki.com/*", "https://www.netflix.com/*"])
@@ -10,28 +11,37 @@ def handle():
     print("Got a request!")
     return "<p>Hello world!<p>"
 
-def getFilename(movieName : str, ep : str, content_type : str):
+def getFilename(movie_obj, content_type : str):
     """ filetype - http filetype """
     # Get name part
+    movieName = movie_obj["movieName"]
+    ep = movie_obj["ep"]
+    lang = movie_obj["lang"]
+
     name = movieName.lower().replace(" ", "-")
     print("Got", content_type)
 
     # Get extension
-    if content_type == "text/xml":
+    if "text/xml" in content_type:
         ext = "xml"
-    elif content_type == "text/vtt":
+    elif "text/vtt" in content_type:
         ext = "vtt"
     else:
         ext = "txt"
 
-    return f'data/{name}-{ep}.{ext}'
+    dir = f'data/{lang}'
+    file = f'{name}-{ep}.{ext}'
+    return dir, file 
 
 @app.post("/")
 def handle_post():
     print("Got post request")
     movieObj = request.json
     vttRes = requests.get(movieObj['url'])
-    filename = getFilename(movieObj["movieName"], movieObj["ep"], vttRes.headers['content-type'])
+    dir, file = getFilename(movieObj, vttRes.headers['content-type'])
+    if not os.path.isdir(dir):
+        os.makedirs(dir)
+    filename = os.path.join(dir,file)
     with open(filename, "wb") as f:
         f.write(vttRes.content)
     print(f'Wrote out {filename}')
