0c4346ad9670f8ef152328eb54401a9ea2d18596
Zachary Lau <zlau2000@gmail.com>
2023-11-02 15:46:37 -0600

    Add updating of single word

--- database.py
+++ database.py
@@ -4,6 +4,7 @@ Includes handlers to register files with the database
 
 import psycopg2
 from configparser import ConfigParser
+import csv
 
 def config(filename='database.ini', section='postgresql'):
     """Parse a config file and return a dictionary of credentiali values"""
@@ -20,6 +21,9 @@ def config(filename='database.ini', section='postgresql'):
     return res
 
 class dbConnection:
+    """
+    Usage specific db connection
+    """
     def __init__(self, inifile):
         creds = config(inifile)
         self.conn = psycopg2.connect(**creds)
@@ -34,8 +38,34 @@ class dbConnection:
             with self.conn.cursor() as curs:
                 for s in statements:
                     curs.execute(s)
-                    res.extend(curs.fetchall())
+                res.append(curs.fetchall())
         return res
+    def get_unique_words(self):
+        return [x[0] for x in self.execute("""SELECT DISTINCT WORD FROM WORDS;""")]
+    def check_exists(self, word):
+        """ Check if a word exists in the db"""
+        res = self.execute(f"""SELECT 1 FROM WORDS WHERE word = '{word}';""")
+        return len(res) > 0
+    def update_word(self, word, count):
+        """Update the count of a single word in the db. Add it if it doesn't yet exist"""
+        with self.conn:
+            with self.conn.cursor() as curs:
+                curs.execute(f"""SELECT WORD, NUM FROM WORDS WHERE word = '{word}';""")
+                res = curs.fetchall()
+                if len(res) > 0:
+                    _, old_count = res[0]
+                    new_count = old_count + count
+                    curs.execute(f"""UPDATE WORDS SET num = {new_count} WHERE word = '{word}' RETURNING *;""")
+                else: # In this case we need a insert
+                    curs.execute(f"""INSERT INTO WORDS (word, num) values ('{word}', {count}) RETURNING *;""")
+                res = curs.fetchall()
+        return res
+        
+    def import_file(filename):
+        with open(filename, 'r') as f:
+            r = csv.reader(f)
+            for row in r:
+                pass
     def close(self):
         self.conn.close()
 
@@ -43,10 +73,12 @@ if __name__ == '__main__':
     try:
         db = dbConnection('database.ini')
         # res = db.execute('SELECT * FROM WORDS;')
-        res = db.execute_list([
-            """SELECT * FROM WORDS WHERE word = '학생';""",
-            """SELECT * FROM WORDS WHERE word = '엄마';"""
-        ])
+        # res = db.execute_list([
+        #     """SELECT * FROM WORDS WHERE word = '학생';""",
+        #     """SELECT * FROM WORDS WHERE word = '엄마';"""
+        # ])
+        # res = db.check_exists('개새끼')A
+        res = db.update_word('졍국 사랑해', 100)
         print(res)
     finally:
         db.close()
