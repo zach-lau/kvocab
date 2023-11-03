59bf82e907de4928775ca0d6d864b4e0ee77afa1
Zachary Lau <zlau2000@gmail.com>
2023-11-02 18:37:23 -0600

    Allow committig multiple

--- database.py
+++ database.py
@@ -47,7 +47,8 @@ class dbConnection:
         res = self.execute(f"""SELECT 1 FROM WORDS WHERE word = '{word}';""")
         return len(res) > 0
     def update_word(self, word, count):
-        """Update the count of a single word in the db. Add it if it doesn't yet exist"""
+        """Deprecated - neds updating for POS 
+        Update the count of a single word in the db. Add it if it doesn't yet exist"""
         with self.conn:
             with self.conn.cursor() as curs:
                 curs.execute(f"""SELECT WORD, NUM FROM WORDS WHERE word = '{word}';""")
@@ -61,30 +62,54 @@ class dbConnection:
                 res = curs.fetchall()
         return res
     def update_many(self, word_list):
+        """ Update the word count of a list of words (unique on word) in the db """
         with self.conn:
             with self.conn.cursor() as curs:
-                # Prep work to get all the words
-                curs.execute(f"""SELECT WORD, NUM FROM WORDS;""")
+                # Prep work to get all the words currently in the table
+                curs.execute(f"""SELECT WORD, POS, NUM FROM WORDS;""")
                 res = curs.fetchall()
-                word_dict = {}
+                current_dict = {}
                 for row in res:
-                    word, num = row
-                    word_dict[word] = num
+                    word, pos, num = row
+                    key = (word, pos)
+                    current_dict[key] = num
+
+                # Get a list of "old" words we already have in anki
+                curs.execute(f"""SELECT WORD FROM OLD_WORDS""")
+                res = curs.fetchall()
+                old_set = set([x[0] for x in res])
+
+                # This is where we sort the rows into update vs insert
                 update_list = []
                 insert_list = []
-                for elem in word_list:
-                    word, num = elem
-                    if word in word_dict.keys():
-                        new_num = word_dict[word] + num
-                        update_list.append((new_num, word)) # Backwards order
+                for row in word_list:
+                    word, pos, ex, num = row
+                    key = (word, pos)
+                    if key in current_dict.keys():
+                        update_list.append(row)
                     else: # New word
-                        insert_list.append((word, num))
-                print(update_list)
-                print(insert_list)
-                update_sql = f"""UPDATE WORDS SET num = %s WHERE word = %s;"""
-                insert_sql = f"""INSERT INTO WORDS (WORD, NUM) VALUES (%s, %s);"""
-                curs.executemany(update_sql, update_list)
-                curs.executemany(insert_sql, insert_list)
+                        insert_list.append(row) # 4 elements
+                # print(update_list)
+                # print(insert_list)
+                update_sql = f"""UPDATE WORDS SET num = %s WHERE word = %s AND pos = %s;"""
+                insert_sql = f"""INSERT INTO WORDS (WORD, POS, EXAMPLE, TYPE, NUM) 
+                                 VALUES (%s, %s, %s, %s, %s);"""
+                def update_tuple(row):
+                    """ Create an approriate update tuple from a row """
+                    word, pos, ex, num = row
+                    old_num = current_dict[(word, pos)]
+                    num += old_num
+                    return (num, word, pos)
+                def insert_tuple(row):
+                    """Create an appropriate sql insert tuple from row"""
+                    word, pos, ex, num = row
+                    if word in old_set:
+                        type = 1 # OLD
+                    else:
+                        type = 2 # NEW
+                    return (word, pos, ex, type, num)
+                curs.executemany(update_sql, map(update_tuple, update_list))
+                curs.executemany(insert_sql, map(insert_tuple, insert_list))
         
     def import_file(filename):
         with open(filename, 'r') as f:
@@ -104,7 +129,11 @@ if __name__ == '__main__':
         # ])
         # res = db.check_exists('개새끼')A
         # res = db.update_word('졍국 사랑해', 100)
-        db.update_many([('정국', 1), ('지민', 10)])
+        db.update_many([
+            ('정국', 'Noun', '', 1), 
+            ('지민', 'Noun', '지민을 사랑한다', 10),
+            ('학생', 'Noun', '난 학생이애요', 1)
+            ])
         # print(res)
     finally:
         db.close()
